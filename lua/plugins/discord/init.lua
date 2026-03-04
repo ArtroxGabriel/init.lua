db958ef98e3215ede58e8fbe13c3b5988535b7c0
Gabrigas <artroxgabriel@gmail.com>
2026-03-04 12:58:14 -0300

    remove url from discord presence

--- lua/plugins/discord/init.lua
+++ lua/plugins/discord/init.lua
@@ -12,7 +12,7 @@ return {
         debounce_timeout = 10,
         enable_line_number = false,
         blacklist = {},
-        buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+        buttons = false, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
         file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
         show_time = true, -- Show the timer
 
